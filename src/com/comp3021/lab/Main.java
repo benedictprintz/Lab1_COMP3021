8da667ec604a007ccc0ab2d02ea467bd969000c1
Benedict Printz <benedictprintz@gmail.com>
2018-09-07 14:23:47 +0800

    The first commit.

---
+++ src/com/comp3021/lab/Main.java
@@ -0,0 +1,13 @@
+package com.comp3021.lab;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        //TODO: Text JAVA10 features
+        var x = 5;
+        System.out.println(x);
+    }
+}
